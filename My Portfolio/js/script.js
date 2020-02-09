ad1fbffdea0732919475822c6d96035426bd87b5
Hafeezur Rahman <53285091+Hafeezistic@users.noreply.github.com>
2020-02-09 16:28:35 +0530

    Add files via upload

---
+++ My Portfolio/js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+	document.getElementById('sidenav').style.width = '50%';
+	document.getElementById('mask').style.width = '50%';
+}
+
+function closeNav() {
+	document.getElementById('sidenav').style.width = '0%';
+	document.getElementById('mask').style.width = '0%';
+}
